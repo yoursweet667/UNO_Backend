9fab90355ad7fa7d58dd1b499f4eb68933f32238
Luminu Seele <>
2021-05-31 21:14:55 +0300

    fix merge conflict

--- UNO/src/main/java/ru/yoursweet667/uno/web/controller/EventController.java
+++ UNO/src/main/java/ru/yoursweet667/uno/web/controller/EventController.java
@@ -1,8 +1,10 @@
 package ru.yoursweet667.uno.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yoursweet667.uno.service.event.EventService;
+import ru.yoursweet667.uno.service.model.event.Event;
 
 @RestController
 public class EventController {
@@ -10,7 +12,6 @@ public class EventController {
     @Autowired
     private EventService eventService;
 
-<<<<<<< HEAD
     @PostMapping("games/{gameId}/events")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void createEvent(@PathVariable("gameId") String gameId, @RequestBody Event event){
@@ -18,10 +19,7 @@ public class EventController {
         eventService.createEvent(gameId, event);
     }
 
-    @PostMapping("games/{gameId}/events")
-=======
     @GetMapping("games/{gameId}/events")
->>>>>>> caa2cae... создал каркас нового API, создал модели и процессоры для внешних команд
     public void getEvents(@PathVariable("gameId") String gameId, @RequestParam String playerId,
                           @RequestParam Integer fromEventId){
 
