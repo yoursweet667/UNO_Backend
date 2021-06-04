fca105467bdc3524a31dca53877cf4b9dc96414e
Luminu Seele <>
2021-06-04 19:01:18 +0300

    Внесение изменений в схему и пример Game в API, импорт одного класса в idea

--- UNO/src/main/java/ru/yoursweet667/uno/web/controller/EventController.java
+++ UNO/src/main/java/ru/yoursweet667/uno/web/controller/EventController.java
@@ -1,6 +1,7 @@
 package ru.yoursweet667.uno.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yoursweet667.uno.service.event.EventService;
 import ru.yoursweet667.uno.service.model.event.Event;
